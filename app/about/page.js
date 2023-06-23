720943f3f3c3dc46471247bb9a4b63dfcc017184
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-06-24 00:58:04 +0100

    Did about page, added 3 card for description Created simple footer

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,5 @@
+import "remixicon/fonts/remixicon.css";
+
 export const metadata = {
   title: "About - BStore",
 };
@@ -20,8 +22,42 @@ const About = () => {
                 </p>
 
                 <button className="px-4 py-2 mt-2 text-green-700 duration-100 border border-green-600 rounded-tl-2xl rounded-br-2xl hover:bg-green-600 hover:text-green-50">
-                  Learn More
+                  Learn More <i className="ri-leaf-line"></i>
                 </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="my-16">
+            <div className="flex flex-wrap justify-evenly">
+              <div className="grid max-w-xs p-6 m-4 mx-auto text-center rounded-md shadow-md place-content-center">
+                <i className="text-5xl text-green-400 ri-time-line"></i>
+                <div className="mt-3">
+                  <h3 className="text-lg font-semibold">Active 24/7</h3>
+                  <p>
+                    Our Customer Service agents are always available to attend to any of your complaints, need or open
+                    suggestions 😇🥳
+                  </p>
+                </div>
+              </div>
+              <div className="grid max-w-xs p-6 m-4 mx-auto text-center rounded-md shadow-md place-content-center">
+                <i className="text-5xl text-red-400 ri-phone-fill"></i>
+                <div className="mt-3">
+                  <h3 className="text-lg font-semibold">Response Time</h3>
+                  <p>
+                    We <span className="font-bold text-green-800">@Bstore</span> response quickly to any request sent by our users and visitors, to give you the users the
+                    best Xperience on our website. 😎
+                  </p>
+                </div>
+              </div>
+              <div className="grid max-w-xs p-6 m-4 mx-auto text-center rounded-md shadow-md place-content-center">
+                <i className="text-5xl text-green-400 ri-shopping-cart-2-line"></i>
+                <div className="mt-3">
+                  <h3 className="text-lg font-semibold">Shipping</h3>
+                  <p>
+                    We ship products worldwide and as fast as possible, we got you covered at all times 💖
+                  </p>
+                </div>
               </div>
             </div>
           </div>
---
+++ app/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-5 mt-5 text-center border-t border-green-300">
+          <p className="text-sm text-green-800">@BStore 2022 - 2023</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;

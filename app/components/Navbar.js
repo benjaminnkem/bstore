4a0606c9134c6e8fd3ce17340fbecf351d8f9be3
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-06-26 13:38:42 +0100

    Changed Navbar Layout Added social media icons

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -10,53 +10,64 @@ const Navbar = () => {
 
   useEffect(() => {
     setAllItems(localStorage.getItem("cartItems"));
-  }, [allItems])
+  }, [allItems]);
 
   return (
     <>
-      <nav>
-        <ul className="flex items-center justify-center py-8 space-x-8 select-none md:py-14 text-slate-900 dark:text-slate-50">
-          <Link href="/">
-            <li
-              className={`relative font-semibold duration-100 dark:hover:text-green-200 hover:text-green-700 ${
-                pathname === "/" ? "border-b-4 border-green-500 text-lg" : "n-links"
-              }`}
-            >
-              Home
-            </li>
-          </Link>
-          <Link href="/shop">
-            <li
-              className={`relative font-semibold duration-100 dark:hover:text-green-200 hover:text-green-700 ${
-                pathname === "/shop" ? "border-b-4 border-green-500 text-lg" : "n-links"
-              }`}
-            >
-              Shop
-            </li>
-          </Link>
-          <Link href="/about">
-            <li
-              className={`relative font-semibold duration-100 dark:hover:text-green-200 hover:text-green-700 ${
-                pathname === "/about" ? "border-b-4 border-green-500 text-lg" : "n-links"
-              }`}
-            >
-              About
-            </li>
-          </Link>
-          <Link href="/contact">
-            <li
-              className={`relative font-semibold duration-100 dark:hover:text-green-200 hover:text-green-700 ${
-                pathname === "/contact" ? "border-b-4 border-green-500 text-lg" : "n-links"
-              }`}
-            >
-              Contact
-            </li>
-          </Link>
-          {/* <li className={`font-semibold duration-100 dark:hover:text-green-200 hover:text-green-700 relative`}>
-            <i className="text-2xl ri-shopping-cart-2-line" id="nav-cart" data-shopping-items={`0`}></i>
-          </li> */}
-        </ul>
-      </nav>
+      <div>
+        <div className="md:max-w-[1024px] w-11/12 mx-auto">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-bold">BStore</h1>
+            </div>
+            <nav>
+              <ul className="flex items-center justify-center py-4 space-x-4 select-none text-slate-900 dark:text-slate-50">
+                <Link href="/">
+                  <li
+                    className={`relative font-semibold duration-100 dark:hover:text-green-200 px-4 py-[6px] rounded-md hover:text-green-700 ${
+                      pathname === "/" ? "bg-green-400 bg-opacity-50" : ""
+                    }`}
+                  >
+                    Home
+                  </li>
+                </Link>
+                <Link href="/shop">
+                  <li
+                    className={`relative font-semibold duration-100 dark:hover:text-green-200 px-4 py-[6px] rounded-md hover:text-green-700 ${
+                      pathname === "/shop" ? "bg-green-400 bg-opacity-50" : ""
+                    }`}
+                  >
+                    Shop
+                  </li>
+                </Link>
+                <Link href="/about">
+                  <li
+                    className={`relative font-semibold duration-100 dark:hover:text-green-200 px-4 py-[6px] rounded-md hover:text-green-700 ${
+                      pathname === "/about" ? "bg-green-400 bg-opacity-50" : ""
+                    }`}
+                  >
+                    About
+                  </li>
+                </Link>
+                <Link href="/contact">
+                  <li
+                    className={`relative font-semibold duration-100 dark:hover:text-green-200 px-4 py-[6px] rounded-md hover:text-green-700 ${
+                      pathname === "/contact" ? "bg-green-400 bg-opacity-50 " : ""
+                    }`}
+                  >
+                    Contact
+                  </li>
+                </Link>
+              </ul>
+            </nav>
+            <div className="flex items-center space-x-3">
+              <i className="text-2xl duration-200 cursor-pointer hover:text-blue-600 ri-facebook-fill"></i>
+              <i className="text-2xl duration-200 cursor-pointer hover:text-red-600 ri-instagram-fill"></i>
+              <i className="text-2xl duration-200 cursor-pointer hover:text-cyan-600 ri-telegram-fill"></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };

17307066cb7bd7d542eb79ea47377f22407237ba
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-07-30 02:45:53 +0100

    Fixed test shop api

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,3 +1,4 @@
+"use client";
 import DefaultWrapper from "../DefaultWrapper";
 import ProductDisplay from "./components/ProductDisplay";
 
@@ -5,23 +6,25 @@ export const metadata = {
   title: "Shop - BStore",
 };
 
-export async function getItems() {
-  const response = await fetch(`${process.env.BASE_URL}/api/test-shop`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch items");
-  }
-  return response.json();
-}
-
 const Shop = async () => {
-  const items = await getItems();
+  let items;
+  async function getItems() {
+    const response = await fetch(`/api/test-shop`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch items");
+    }
+    return response.json();
+  }
+
+  items = await getItems();
+
   return (
     <>
-    <DefaultWrapper>
-      <div className="my-10">
-        <ProductDisplay items={items} />
-      </div>
-    </DefaultWrapper>
+      <DefaultWrapper>
+        <div className="my-10">
+          <ProductDisplay items={items} />
+        </div>
+      </DefaultWrapper>
     </>
   );
 };

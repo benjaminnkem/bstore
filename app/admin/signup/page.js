bf377ea106c8b60b351d2c327e19a718cf2c5326
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-08-28 14:13:45 +0100

    Concluded User signup with validation

--- app/admin/signup/page.js
+++ app/admin/signup/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { sun3D } from "@/app/reuseable/three/solar/solar";
-import { faRocket } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faRocket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -11,7 +11,8 @@ import Link from "next/link";
 
 const CreateAdmin = () => {
   const [formInput, setFormInput] = useState({ username: "", password: "", email: "" });
-  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState({ loading: false, success: false, error: false });
+  const [err, setErr] = useState("");
 
   const router = useRouter();
 
@@ -23,12 +24,17 @@ const CreateAdmin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // setLoading(true);
-    // const res = await axios.post("/api/create/new-admin", formInput);
-    // if (res.statusText.toLocaleLowerCase() !== "ok") return;
-    // setLoading(false);
+    setStatus({ ...status, loading: true });
+    try {
+      await axios.post("/api/create/new-user", formInput);
+      setStatus({ ...status, loading: false, success: true });
+    } catch (e) {
+      setStatus({ ...status, loading: false, error: true });
+      setErr(e.response.statusText);
+      return;
+    }
 
-    // router.push("/admin/login");
+    // router.push("/admin/login")
   };
 
   useEffect(() => sun3D(), []);
@@ -36,72 +42,115 @@ const CreateAdmin = () => {
   return (
     <>
       <div>
-        <div className="md:fixed md:w-1/2 w-full bg-opacity-80 duration-200 bg-[#212121] min-h-full grid place-content-center absolute">
-          <div className="p-4">
-            <div>
-              <h1 className="text-4xl pb-6 font-bold">
-                Create An <span className="text-orange-500">Account</span>
-              </h1>
-            </div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-              <div className="space-y-5">
-                <div>
-                  <label htmlFor="username">Username</label>
-                  <input
-                    type="text"
-                    className="w-full bg-transparent border-b border-slate-400 border-opacity-20 p-2 outline-none"
-                    placeholder="Username"
-                    autoComplete="off"
-                    name="username"
-                    value={formInput.username}
-                    onChange={(e) => handleFormInput(e)}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="email">Email</label>
-                  <input
-                    type="text"
-                    className="w-full bg-transparent border-b border-slate-400 border-opacity-20 p-2 outline-none"
-                    placeholder="Enter email"
-                    name="email"
-                    value={formInput.email}
-                    onChange={(e) => handleFormInput(e)}
-                    autoComplete="off"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="password">Password</label>
-                  <input
-                    type="password"
-                    className="w-full bg-transparent border-b border-slate-400 border-opacity-20 p-2 outline-none"
-                    placeholder="Password"
-                    autoComplete="off"
-                    name="password"
-                    value={formInput.password}
-                    onChange={(e) => handleFormInput(e)}
-                  />
-                </div>
+        <div className="md:fixed md:w-1/2 w-full bg-opacity-90 duration-200 bg-[#212121] min-h-full grid place-content-center absolute">
+          <Link
+            href={"/"}
+            passHref
+            className="absolute top-4 left-4 duration-200 md:text-lg text-base px-2 py-[2px] hover:border-[#656565] border border-transparent rounded-md"
+          >
+            <FontAwesomeIcon icon={faArrowLeft} /> Home
+          </Link>
 
-                <div>
-                  <p className="text-sm">
-                    Already have an account? <Link href={'/admin/login'} className="border-b border-orange-400">Login</Link>
-                  </p>
-                </div>
+          <div className="p-4">
+            {status.success ? (
+              <div className="space-y-2 md:text-start text-center">
+                <h3 className="text-3xl">Account Created Successfully! 🎉</h3>
                 <button
-                  role="submit"
-                  disabled={loading}
-                  className="py-2 rounded-md border-2 border-gray-300 border-opacity-40 w-full duration-200 hover:bg-gray-300 hover:text-gray-900 disabled:opacity-10 disabled:hover:bg-transparent disabled:hover:text-white"
+                  className="mx-auto md:mx-0 px-4 py-2 rounded-md border border-orange-500"
+                  onClick={() => router.push("/admin/login")}
                 >
-                  {!loading ? (
-                    <>
-                      Sign Up <FontAwesomeIcon icon={faRocket} />
-                    </>
-                  ) : (
-                    "Creating..."
-                  )}
+                  Login <FontAwesomeIcon icon={faRocket} />
                 </button>
               </div>
-            </form>
+            ) : status.error ? (
+              <div>
+                <div className="space-y-2 md:text-start text-center">
+                  <h3 className="text-xl font-semibold text-red-300">{err}</h3>
+                  <button
+                    className="mx-auto md:mx-0 px-4 py-2 rounded-md border border-orange-500"
+                    onClick={() => setStatus({ error: false })}
+                  >
+                    Try Again
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <>
+                <div className="pb-6">
+                  <h1 className="text-4xl font-bold">
+                    Create An <span className="text-orange-500">Account</span>
+                  </h1>
+                  {/* {status.success && (
+                    <p className="text-sm text-green-500 font-semibold">Created account successfully! 🎉</p>
+                  )} */}
+                </div>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                  <div className="space-y-5">
+                    <div>
+                      <label htmlFor="username">Username</label>
+                      <input
+                        type="text"
+                        className="w-full bg-transparent border-b border-slate-400 border-opacity-20 p-2 outline-none"
+                        placeholder="Username"
+                        autoComplete="off"
+                        name="username"
+                        value={formInput.username}
+                        onChange={(e) => handleFormInput(e)}
+                        required
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="email">Email</label>
+                      <input
+                        type="text"
+                        className="w-full bg-transparent border-b border-slate-400 border-opacity-20 p-2 outline-none"
+                        placeholder="Enter email"
+                        name="email"
+                        value={formInput.email}
+                        onChange={(e) => handleFormInput(e)}
+                        required
+                        autoComplete="off"
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="password">Password</label>
+                      <input
+                        type="password"
+                        className="w-full bg-transparent border-b border-slate-400 border-opacity-20 p-2 outline-none"
+                        placeholder="Password"
+                        autoComplete="off"
+                        name="password"
+                        value={formInput.password}
+                        onChange={(e) => handleFormInput(e)}
+                        required
+                      />
+                    </div>
+
+                    <div>
+                      <p className="text-sm">
+                        Already have an account?{" "}
+                        <Link href={"/admin/login"} className="border-b border-orange-400">
+                          Login
+                        </Link>
+                      </p>
+                    </div>
+                    <button
+                      role="submit"
+                      disabled={status.loading}
+                      className="py-2 rounded-md border-2 border-gray-300 border-opacity-40 w-full duration-200 hover:bg-gray-300 hover:text-gray-900 disabled:opacity-10 disabled:hover:bg-transparent disabled:hover:text-white"
+                    >
+                      {!status.loading ? (
+                        <>
+                          Sign Up <FontAwesomeIcon icon={faRocket} />
+                        </>
+                      ) : (
+                        "Creating..."
+                      )}
+                    </button>
+                  </div>
+                </form>
+              </>
+            )}
           </div>
         </div>
 
--- app/api/create/new-user/route.js
+++ app/api/create/new-user/route.js
@@ -1,15 +1,36 @@
 import connectToDB from "@/utils/db";
-import User from "@/utils/schemas/users/UsersSchema";
+import UsersSchema from "@/utils/schemas/users/UsersSchema";
+import { hash } from "bcryptjs";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const data = await request.json();
 
   try {
-    await connectToDB();
-    const allUsers = await User.find({});
-    return NextResponse.json({ data, allUsers });
+    const { username, email, password } = data;
+    if (!username || !password || !email) throw new Error("Invalid username or password");
+
+    const userExists = await UsersSchema.findOne({ username });
+    if (userExists)
+      return new NextResponse("User already exists", {
+        status: 405,
+        statusText: `Username (${username}) is unavailable`,
+      });
+
+    const emailExists = await UsersSchema.findOne({ email });
+    if (emailExists)
+      return new NextResponse(`User with email (${email}) already exists`, {
+        status: 405,
+        statusText: `User with email (${email}) already exists`,
+      });
+
+    const hashedPassword = await hash(password, 12);
+
+    connectToDB();
+    await UsersSchema.create({ username, email, password: hashedPassword });
+    return new NextResponse("Success");
   } catch (e) {
     console.log(e);
+    return new NextResponse("Something went wrong", { status: 500 });
   }
 }

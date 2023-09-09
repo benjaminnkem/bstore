cdd2fe9a74da5b963f22ac03dac7d6d75039da55
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-09-09 22:45:07 +0100

    Create tags page

---
+++ app/tags/[tag]/page.js
@@ -0,0 +1,15 @@
+import DefaultWrapper from "@/utils/providers/DefaultWrapper";
+
+const TagsPage = () => {
+  return (
+    <>
+      <DefaultWrapper>
+        <div className="md:max-w-[1488px] w-11/12 mx-auto">
+          <h1>Tags</h1>
+        </div>
+      </DefaultWrapper>
+    </>
+  );
+};
+
+export default TagsPage;

8100e19e7a5d9b7797801a8a4a89b8e479a752e7
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-09-17 00:19:28 +0100

    Changed green in home to orange

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,16 @@
 import "./globals.css";
 import "remixicon/fonts/remixicon.css";
-import { Poppins } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import Providers from "@/utils/providers/providers";
 config.autoAddCss = false;
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "300", "500", "800"],
-});
+const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={poppins.className}>
+    <html lang="en" className={dmSans.className}>
       <body>
         <Providers>{children}</Providers>
       </body>

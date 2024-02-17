dbee6fe3e19c226d24fc35bbd3eb74c00aa63541
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-02-17 09:02:05 +0100

    feat: home hero image slideshow

--- components/UI/Home/header.js
+++ components/UI/Home/header.js
@@ -2,24 +2,49 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { opacityVariant, parentVariant } from "@/lib/utils/variants";
+import { useEffect, useState } from "react";
+
+const images = [
+  { id: 0, path: "home-bg.jpg" },
+  { id: 1, path: "cat-on-bed.jpg" },
+  { id: 2, path: "light-strands.jpg" },
+  { id: 3, path: "pink-cake.jpg" },
+];
 
 const HomeHeader = () => {
+  const [imgIndex, setImgIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log(imgIndex);
+      setImgIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <header className="w-full">
       <div className="w-full min-h-[44rem] flex justify-center items-center relative overflow-hidden">
-        <Image
-          src={"/images/bg/home-bg.jpg"}
-          alt="header cover"
-          width={1920}
-          height={1000}
-          className="absolute top-0 left-0 object-cover w-full h-full -z-20"
-        />
+        {images.map((data, id) => (
+          <>
+            <div key={id} className={`duration-300 -z-20 ${imgIndex === id ? "opacity-100" : "opacity-0"}`}>
+              <Image
+                src={`/images/bg/${data.path}`}
+                alt="header cover"
+                width={1920}
+                height={1000}
+                className="absolute top-0 left-0 object-cover w-full h-full"
+              />
+            </div>
+          </>
+        ))}
 
         <div className="absolute top-0 left-0 w-full h-full -z-10 bg-gradient-to-t from-[#131313] to-transparent"></div>
 
-        <div className="container ">
+        <div className="container">
           <div className="flex items-center text-white">
             <div className="mt-10">
               <motion.div
